b9e3aaeeb5005c21136188c1547a1bbf3cb40214
Ryan Pfister <57775823+ryanpfister@users.noreply.github.com>
2023-10-23 19:25:02 -0400

    new lol

--- index.js
+++ index.js
@@ -14,33 +14,93 @@ const range = 'Sheet1!A1:A1'; // Assuming the value is in cell A1
 // Load the credentials file
 const credentials = require('./credentials.json');
 
-// Configure authentication
-const oAuth2Client = new google.auth.OAuth2();
-const token = fs.readFileSync('./token.json');
-oAuth2Client.setCredentials(JSON.parse(token));
+// Create an OAuth2 client using the credentials
+const oAuth2Client = new google.auth.OAuth2(
+  credentials.client_id,
+  credentials.client_secret,
+  credentials.redirect_uris[0]
+);
 
+// Check if the token.json file exists
+if (fs.existsSync('token.json')) {
+  // Load the token from the file
+  const token = fs.readFileSync('token.json');
+  oAuth2Client.setCredentials(JSON.parse(token));
+}
 
-// Create a GET route
-app.get('/', (req, res) => {
-  // Call the Google Sheets API to get the values
-  const sheets = google.sheets({ version: 'v4', auth: oAuth2Client });
-  sheets.spreadsheets.values.get(
-    {
-      spreadsheetId,
-      range,
-    },
-    (err, response) => {
-      if (err) {
-        console.error('The API returned an error:', err);
-        res.status(500).send('Error fetching data from Google Sheets');
-      } else {
-        const values = response.data.values;
-        const value = values[0][0]; // Assuming there is only one value in the specified range
-        res.json({ value });
-      }
-    }
-  );
+// Generate the authentication URL and log it
+const authUrl = oAuth2Client.generateAuthUrl({
+  access_type: 'offline',
+  scope: [
+    'https://www.googleapis.com/auth/spreadsheets.readonly',
+    'https://www.googleapis.com/auth/spreadsheets',
+  ],
 });
+console.log('Authorize this app by visiting this URL:', authUrl);
+
+// Start the server
+app.listen(80, () => {
+    console.log(`Server is listening on port`);
+  });
+  
+// Create a route to handle the OAuth2 callback
+app.get('/auth/callback', (req, res) => {
+  const code = req.query.code;
+  oAuth2Client.getToken(code, (err, token) => {
+    if (err) {
+      console.error('Error retrieving access token:', err);
+      res.status(500).send('Error retrieving access token');
+      return;
+    }
+    oAuth2Client.setCredentials(token);
+    fs.writeFileSync('token.json', JSON.stringify(token));
+    res.redirect('/');
+  });
+});
+
+app.get('/callback', (req, res) => {
+    const code = req.query.code;
+    // Use the code to exchange for access and refresh tokens with the Google API
+    // Handle the tokens and store them securely for further API requests
+    res.send('Authorization successful! You may close this page.');
+  });
+
+  app.get('/auth', (req, res) => {
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: [
+        'https://www.googleapis.com/auth/spreadsheets.readonly',
+        'https://www.googleapis.com/auth/spreadsheets',
+      ],
+      redirect_uri: 'http://localhost:80/callback',
+    });
+    res.redirect(authUrl);
+  });
+
+app.get('/', (req, res) => {
+    // Call the Google Sheets API to get the values
+    const sheets = google.sheets({ version: 'v4', auth: oAuth2Client });
+    sheets.spreadsheets.values.get(
+      {
+        spreadsheetId,
+        range,
+      },
+      (err, response) => {
+        if (err) {
+          console.error('The API returned an error:', err);
+          res.status(500).send('Error fetching data from Google Sheets');
+        } else {
+          const values = response?.data?.values;
+          if (values && values.length > 0 && values[0].length > 0) {
+            const value = values[0][0];
+            res.send(`Value: ${value}`);
+          } else {
+            res.status(500).send('No value found in the specified range');
+          }
+        }
+      }
+    );
+  });
 
 // Create a POST route to increase the value
 app.post('/addcall', (req, res) => {
@@ -77,7 +137,3 @@ app.post('/addcall', (req, res) => {
   });
 });
 
-// Start the server
-app.listen(80, () => {
-  console.log(`Server is listening on port`);
-});
