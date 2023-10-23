7d35a95cc4a8eba10168b3dafb698f150f19efdc
Ryan Pfister <57775823+ryanpfister@users.noreply.github.com>
2023-10-23 12:39:31 -0400

    f

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 // Create an instance of the Express.js app
 const app = express();
-const port = 3000;
+const port = 80;
 
 // Define the Google Sheets file ID and range
 const spreadsheetId = '1YwHE1GA2HrAoGCUBg7svh_Ol7-N3R1kRB9XkEOqEx90';
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
       } else {
         const values = response.data.values;
         const value = values[0][0]; // Assuming there is only one value in the specified range
-        res.send(`${value}`);
+        res.send(`Current value: ${value}`);
       }
     }
   );
@@ -44,40 +44,40 @@ app.get('/', (req, res) => {
 
 // Create a POST route to increase the value
 app.post('/addcall', (req, res) => {
-    // Call the Google Sheets API to get the current value
-    const sheets = google.sheets({ version: 'v4', auth: oAuth2Client });
-    sheets.spreadsheets.values.get({
-      spreadsheetId,
-      range
-    }, (err, response) => {
-      if (err) {
-        console.error('The API returned an error:', err);
-        res.status(500).send('Error fetching data from Google Sheets');
-      } else {
-        const value = parseInt(response.data.values[0][0]);
-        const newValue = value + 1;
-  
-        // Call the Google Sheets API to update the value
-        sheets.spreadsheets.values.update({
-          spreadsheetId,
-          range,
-          valueInputOption: 'RAW',
-          resource: {
-            values: [[newValue]]
-          }
-        }, (err, response) => {
-          if (err) {
-            console.error('The API returned an error:', err);
-            res.status(500).send('Error updating data in Google Sheets');
-          } else {
-            res.send(`Value increased to: ${newValue}`);
-          }
-        });
-      }
-    });
+  // Call the Google Sheets API to get the current value
+  const sheets = google.sheets({ version: 'v4', auth: oAuth2Client });
+  sheets.spreadsheets.values.get({
+    spreadsheetId,
+    range
+  }, (err, response) => {
+    if (err) {
+      console.error('The API returned an error:', err);
+      res.status(500).send('Error fetching data from Google Sheets');
+    } else {
+      const value = parseInt(response.data.values[0][0]);
+      const newValue = value + 1;
+
+      // Call the Google Sheets API to update the value
+      sheets.spreadsheets.values.update({
+        spreadsheetId,
+        range,
+        valueInputOption: 'RAW',
+        resource: {
+          values: [[newValue]]
+        }
+      }, (err, response) => {
+        if (err) {
+          console.error('The API returned an error:', err);
+          res.status(500).send('Error updating data in Google Sheets');
+        } else {
+          res.send(`Value increased to: ${newValue}`);
+        }
+      });
+    }
   });
+});
 
 // Start the server
-app.listen(3000, () => {
+app.listen(80, () => {
   console.log(`Server is listening on port`);
 });
