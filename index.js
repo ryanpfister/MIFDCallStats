e05d2a8746b1551620ad59d8759921d4c41d21d6
Ryan Pfister <57775823+ryanpfister@users.noreply.github.com>
2023-10-25 14:30:32 -0400

    Calls from Rednmx api

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const axios = require('axios');
 const debug = require('debug')('app:server');
 
 const app = express();
 const port = 80;
-const connectionURL = 'mongodb+srv://mifd413:k72CBvWHm4TvN3h8@calldata.vbgrvc3.mongodb.net/CallData?retryWrites=true&w=majority';
+const connectionURL =
+    'mongodb+srv://mifd413:k72CBvWHm4TvN3h8@calldata.vbgrvc3.mongodb.net/CallData?retryWrites=true&w=majority';
 const databaseName = 'CallData';
 const collectionName = 'MIFDCalls';
 
@@ -13,62 +15,159 @@ let collection;
 
 // Connect to MongoDB
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((client) => {
-    db = client.db(databaseName);
-    collection = db.collection(collectionName);
-    console.log('Connected to MongoDB');
-    app.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
-    });
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-    process.exit(1);
-  });
+    .then((client) => {
+        db = client.db(databaseName);
+        collection = db.collection(collectionName);
+        console.log('Connected to MongoDB');
+        // Make initial POST request to retrieve all 911 calls
+        axios
+            .post('http://104.167.248.66:8866/dispcall.json.php', 'persid=539&fdid=52068')
+            .then((response) => {
+                const dispcall = response.data.dispcall;
+                const callIds = dispcall.map(call => call.call_id);
 
-// Handle GET request to '/'
-app.get('/', (req, res) => {
-  collection
-    .findOne({})
-    .then((result) => {
-      if (result) {
-        res.json({ value: result.value });
-      } else {
-        res.status(404).send('No data found');
-      }
+                // Find calls that do not exist in the database
+                const existingCallsPromise = collection.find({ call_id: { $in: callIds } }).toArray();
+                const newCalls = dispcall.filter(call => !callIds.includes(call.call_id));
+
+                existingCallsPromise
+                    .then(existingCalls => {
+                        const existingCallIds = existingCalls.map(call => call.call_id);
+
+                        // Filter out any calls that already exist in the database
+                        const filteredCalls = newCalls.filter(call => !existingCallIds.includes(call.call_id));
+
+                        if (filteredCalls.length > 0) {
+                            const insertPromises = filteredCalls.map(call => collection.insertOne(call));
+
+                            Promise.all(insertPromises)
+                                .then((results) => {
+                                    const insertedCount = results.length;
+                                    console.log(`Inserted ${insertedCount} new calls into the database`);
+                                })
+                                .catch((error) => {
+                                    console.error('Error inserting new calls:', error);
+                                });
+                        } else {
+                            console.log('No new calls found');
+                        }
+                    })
+                    .catch((error) => {
+                        console.error('Error retrieving existing calls:', error);
+                    });
+            })
+            .catch((error) => {
+                console.error('Error retrieving new calls:', error);
+            });
+
+
+
+        setInterval(() => {
+            axios
+                .post('http://104.167.248.66:8866/dispcall.json.php', 'persid=539&fdid=52068')
+                .then((response) => {
+                    // Check the structure of the response
+                    //console.log(response.data);
+                    const newCalls = response.data.dispcall;
+
+                    if (!Array.isArray(newCalls)) {
+                        console.error('Received response without dispcall array');
+                        return;
+                    }
+
+                    // Save only the new calls to the database
+                    collection.find().toArray()
+                        .then((existingCalls) => {
+                            const existingCallIds = existingCalls.map(call => call.call_id);
+
+                            const filteredCalls = newCalls.filter(call => !existingCallIds.includes(call.call_id));
+
+                            if (filteredCalls.length > 0) {
+                                const insertPromises = filteredCalls.map(call => collection.insertOne(call));
+
+                                Promise.all(insertPromises)
+                                    .then((results) => {
+                                        const insertedCount = results.length;
+                                        console.log(`Inserted ${insertedCount} new calls into the database`);
+                                    })
+                                    .catch((error) => {
+                                        console.error('Error inserting new calls:', error);
+                                    });
+                            } else {
+                                console.log('No new calls found');
+                            }
+                        })
+                        .catch((error) => {
+                            console.error('Error retrieving existing calls:', error);
+                        });
+                })
+                .catch((error) => {
+                    console.error('Error retrieving new calls:', error);
+                });
+        }, 30000); // 30 seconds
+
+
+
+
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`);
+        });
     })
     .catch((err) => {
-      console.error('Error retrieving data from MongoDB:', err);
-      res.status(500).send('Error fetching data from MongoDB');
+        console.error('Error connecting to MongoDB:', err);
+        process.exit(1);
     });
-});
 
-// Handle POST request to '/addcall'
-app.post('/addcall', (req, res) => {
+// Handle calls request
+app.get('/api/calls', (req, res) => {
+    const limit = 10; // Number of recent calls to fetch
+  
     collection
-      .findOne({}, { value: 1 })
-      .then((result) => {
-        let newValue = 1;
-        if (result) {
-          newValue = result.value + 1;
-          collection.updateOne({}, { $set: { value: newValue } })
-            .then(() => res.json({ value: newValue }))
-            .catch((err) => {
-              console.error('Error updating data in MongoDB:', err);
-              res.status(500).send('Error updating data in MongoDB');
-            });
-        } else {
-          collection.insertOne({ value: 1 })
-            .then(() => res.json({ value: newValue }))
-            .catch((err) => {
-              console.error('Error inserting data into MongoDB:', err);
-              res.status(500).send('Error inserting data into MongoDB');
-            });
-        }
+      .find()
+      .sort({ call_id: -1 }) // Sort in descending order of call_id to get the most recent calls
+      .limit(limit)
+      .toArray()
+      .then((calls) => {
+        res.json(calls);
       })
-      .catch((err) => {
-        console.error('Error retrieving data from MongoDB:', err);
-        res.status(500).send('Error retrieving data from MongoDB');
+      .catch((error) => {
+        console.error('Error retrieving recent calls:', error);
+        res.status(500).json({ error: 'Internal server error' });
       });
   });
   
+
+// Endpoint for getting call statistics
+app.get('/api/stats', (req, res) => {
+    collection.aggregate([
+      {
+        $group: {
+          _id: null,
+          total: { $sum: 1 },
+          openCalls: {
+            $sum: {
+              $cond: [{ $eq: ["$status", "Open"] }, 1, 0]
+            }
+          },
+          closedCalls: {
+            $sum: {
+              $cond: [{ $eq: ["$status", "Closed"] }, 1, 0]
+            }
+          },
+          callsToDate: { $max: "$call_date" } // Assuming you have a "call_date" field in your collection
+        }
+      }
+    ]).toArray()
+      .then((result) => {
+        const stats = result[0];
+        res.json(stats);
+      })
+      .catch((error) => {
+        console.error('Error retrieving call statistics:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      });
+  });
+  
+
+
+app.use(express.static('public'));
---
+++ public/main.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const callsList = document.getElementById('calls-list');
+  const statsContainer = document.getElementById('stats-container');
+
+  function displayCalls(calls) {
+    callsList.innerHTML = '';
+
+    calls.forEach(call => {
+      const callItem = document.createElement('li');
+      callItem.innerHTML = `
+        <div>
+          <p><strong>Call ID:</strong> ${call.incnum}</p>
+          <p><strong>Call Type:</strong> ${call.dispcalltypedescr} - ${call.dispsubtypedescr}</p>
+          <p><strong>Location Name:</strong> ${call.sitename}</p>
+          <p><strong>Location:</strong> ${call.address} - ${call.city} - ${call.state} - ${call.zip}</p>
+          <p><strong>Cross:</strong> CROSS 1: ${call.cross1} | CROSS 2: ${call.cross2}</p>
+        </div>
+        <hr>
+      `;
+
+      callsList.appendChild(callItem);
+    });
+  }
+
+  function displayStats(stats) {
+    statsContainer.innerHTML = '';
+  
+    const statsTitle = document.createElement('h2');
+    statsTitle.textContent = 'Call Statistics';
+    statsContainer.appendChild(statsTitle);
+  
+    const numCalls = document.createElement('p');
+    numCalls.textContent = `Number of Calls: ${stats.total}`;
+    statsContainer.appendChild(numCalls);
+  
+    if (stats.calls && Array.isArray(stats.calls)) {
+      stats.calls.forEach(call => {
+        const callType = document.createElement('p');
+        const icon = call.icon;
+  
+        if (icon === 'EMS') {
+          callType.textContent = `Call Type: EMS`;
+        } else if (icon === 'FIRE') {
+          callType.textContent = `Call Type: FIRE`;
+        } else {
+          callType.textContent = `Call Type: Unknown`;
+        }
+  
+        statsContainer.appendChild(callType);
+      });
+    } else {
+      const noCalls = document.createElement('p');
+      noCalls.textContent = 'No call statistics available.';
+    }
+  }
+
+  
+
+  function fetchRecentCalls() {
+    fetch('/api/calls')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Received calls response:', data);
+        displayCalls(data);
+      })
+      .catch(error => {
+        console.error('Error fetching recent calls:', error);
+      });
+  }
+
+  function fetchCallStats() {
+    fetch('/api/stats')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Received stats response:', data);
+        displayStats(data);
+      })
+      .catch(error => {
+        console.error('Error fetching call statistics:', error);
+      });
+  }
+
+  fetchRecentCalls();
+  fetchCallStats();
+});
